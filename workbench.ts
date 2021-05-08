07d3dd9f45bbab3e4b9776be9f2c9e2512f03443
Félix BOROT <fborot@divalto.com>
2021-05-08 14:48:09 +0200

    Upgrade 1.56.0

--- workbench.ts
+++ workbench.ts
@@ -1,14 +1,14 @@
-import { create, IWorkbenchConstructionOptions, IWorkspaceProvider } from 'vs/workbench/workbench.web.api';
-import { URI, UriComponents} from 'vs/base/common/uri';
+import { create, IWorkbenchConstructionOptions, IWorkspaceProvider, IWorkspace } from 'vs/workbench/workbench.web.api';
+import { URI, UriComponents } from 'vs/base/common/uri';
 declare const window: any;
 
 (async function () {
 	// create workbench
-	let config: IWorkbenchConstructionOptions & { folderUri?: UriComponents, workspaceUri?: UriComponents }  = {};
+	let config: IWorkbenchConstructionOptions & { folderUri?: UriComponents, workspaceUri?: UriComponents } = {};
 
-	if(window.product){
+	if (window.product) {
 		config = window.product;
-	}else{
+	} else {
 		const result = await fetch('/product.json');
 		config = await result.json();
 	}
@@ -29,8 +29,12 @@ declare const window: any;
 		workspace = undefined;
 	}
 
-	if(workspace){
-		const workspaceProvider: IWorkspaceProvider = { workspace, open: async () => {}, trusted: true }
+	if (workspace) {
+		const workspaceProvider: IWorkspaceProvider = { 
+			workspace,
+			open: async (workspace: IWorkspace, options?: { reuse?: boolean, payload?: object }) => true,
+			trusted: true 
+		}
 		config = { ...config, workspaceProvider };
 	}
 
