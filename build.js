7d4e1b947509dbda45483835bed21c43002c74e1
Félix BOROT <fborot@divalto.com>
2022-07-19 14:58:02 +0200

    upgrade 1.69.2

--- build.js
+++ build.js
@@ -3,7 +3,7 @@ const child_process = require("child_process");
 const fs = require("fs");
 const fse = require("fs-extra");
 
-const vscodeVersion = "1.69.0";
+const vscodeVersion = "1.69.2";
 
 if (!fs.existsSync("vscode")) {
   child_process.execSync(`git clone --depth 1 https://github.com/microsoft/vscode.git -b ${vscodeVersion}`, {
