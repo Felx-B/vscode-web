16e0afbf10b0344a101d3dc52534130c70fb9302
Thorsten Wolf <thorsten.wolf@neptune-software.com>
2024-07-15 13:53:51 +0200

    implemented feedback

--- build.js
+++ build.js
@@ -1,10 +1,10 @@
-const fs = require("fs");
-const path = require("path");
-const assert = require("assert");
+const process = require("process");
 const child_process = require("child_process");
+const fs = require("fs");
+const fse = require("fs-extra");
 const { version } = require("./package.json");
 
-const VSCODE_VERSION = version.split("-")[0];
+const vscodeVersion = version.split("-")[0];
 
 function error(msg) {
   console.info("\x1b[31merror %s\x1b[0m", msg)
@@ -41,12 +41,12 @@ if (node_version < "v20.0") {
 
 if (!fs.existsSync("vscode")) {
   note("cloning vscode")
-  exec(`git clone --depth 1 https://github.com/microsoft/vscode.git -b ${VSCODE_VERSION}`, {
+  exec(`git clone --depth 1 https://github.com/microsoft/vscode.git -b ${vscodeVersion}`, {
     stdio: "inherit",
   });
 } else {
-  ok("vscodium already installed")
-  note("delete vscodium folder to clone again")
+  ok("vscode already installed")
+  note("delete vscode folder to clone again")
 }
 
 note("changing directory to vscode")
@@ -70,56 +70,11 @@ ok("compile completed")
 // Extract compiled files
 if (fs.existsSync("../dist")) {
   note("cleaning ../dist")
-  fs.rmSync("../dist", { recursive: true, force: true });
+  fs.rmdirSync("../dist", { recursive: true });
 } else {
   ok("../dist did not exist. No need to clean")
 }
 
 fs.mkdirSync("../dist");
-fs.cpSync("../vscode-web", "../dist", { recursive: true, force: true });
+fse.copySync("../vscode-web", "../dist");
 ok("copied ../vscode-web to ../dist")
-
-// Patch output
-note("beginning complex patches for networking")
-const distWorkbenchPath = "../dist/out/vs/workbench/workbench.web.main.js";
-const workbench = fs.readFileSync(distWorkbenchPath, "utf8");
-const workbenchPatched = workbench.replace(/("https:\/\/{{uuid}}[^"]+")/g, "(globalThis.VSCODE_WEB_PUBLIC_URL||$1)");
-fs.writeFileSync(distWorkbenchPath, workbenchPatched, "utf8");
-
-// Strip sourcemaps and patch vscode-uri for "://" URIs
-const re = /.=function\(\)\{function .\((.),.,(.),.,(.),.\)\{/g;
-const patchOutput = async (destPath) => {
-  let hasPatched = false;
-  let hasStripped = false;
-  const stat = await fs.promises.stat(destPath);
-  if (stat.isDirectory()) {
-    for (const child of await fs.promises.readdir(destPath)) {
-      hasStripped |= await patchOutput(path.join(destPath, child));
-    }
-  }
-  if (stat.isFile() && destPath.endsWith(".js")) {
-    const content = await fs.promises.readFile(destPath, "utf8");
-    const stripped = content.replace(/^\/\/# sourceMappingURL=[^\r\n]*/gm, "");
-    if (stripped !== content) {
-      note(`Stripped sourcemaps from ${destPath}`);
-      await fs.promises.writeFile(destPath, stripped, "utf8");
-      hasStripped = true;
-    }
-    if (destPath.includes("jsonServer")) {
-      const matches = re.exec(stripped);
-      if (matches && matches.length === 4) {
-        const [_, e, n, t] = matches;
-        const patched = stripped.replace(
-          re,
-          `${_}if(${n}&&${n}.startsWith("://")){try{let o=globalThis.origin.split("://");${e}=o[0];${t}=o[1];${n}=${n}.replace("://","/")}catch(_){}};`
-        );
-        ok(`Patched ${destPath} for vscode-uri`);
-        await fs.promises.writeFile(destPath, patched, "utf8");
-        hasPatched = true;
-      }
-    }
-  }
-  return hasStripped && hasPatched;
-};
-
-assert(patchOutput(path.join(__dirname, "dist")), "Failed to patch output.");
